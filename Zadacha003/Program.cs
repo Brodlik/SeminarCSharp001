485d4dce9016109bf553ad1c4c3913e03c011818
Svetlana <brodlik@me.com>
2022-07-11 18:50:42 +0300

    WeekDay

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write ("Введите номер для недели: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+switch (num1)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели");
+        break;
+}
