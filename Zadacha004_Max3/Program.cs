25fe13b6159daed7c887d3e8bcec0bd487d35da8
Svetlana <brodlik@me.com>
2022-07-12 14:31:22 +0300

    Zadacha004_max3

---
+++ Zadacha004_Max3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    if (A > C)
+    {
+        Console.Write("max= ");
+        Console.WriteLine(A);
+    }
+    else if (C>A)
+    {
+        Console.Write("max= ");
+        Console.WriteLine(C);
+    }
+}
+if (B > A)
+{
+    if (B > C)
+    {
+        Console.Write("max= ");
+        Console.WriteLine(B);
+    }
+    else if (C>B)
+    {
+        Console.Write("max= ");
+        Console.WriteLine(C);
+    }
+}
