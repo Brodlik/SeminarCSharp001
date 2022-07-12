181d151883c6c39c39c6be7a11872cf9193c58f9
Svetlana <brodlik@me.com>
2022-07-12 14:51:36 +0300

    Zadacha006_chetnoe

---
+++ Zadacha006_Chetnoe/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N%2==0)
+{
+    Console.WriteLine("Да, это четное число");
+
+}
+else{
+    Console.WriteLine ("Нет, это число нечетное");
+}
