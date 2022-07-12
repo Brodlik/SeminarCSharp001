aa2b63376a5139327c91eab2049b8dbbc45138c8
Svetlana <brodlik@me.com>
2022-07-12 13:22:41 +0300

    Zadacha007_Max

---
+++ Zadacha002_Max/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A>B)
+{
+    Console.WriteLine (A);
+}
+else
+{
+    Console.WriteLine (B);
+}
